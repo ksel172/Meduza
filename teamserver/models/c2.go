8e83feff60b4e697fc0f4cc9a45db1e932f9cb0d
mochivi <victormochi1@gmail.com>
2024-11-20 16:58:13 -0300

    c2 model

---
+++ teamserver/models/c2.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// C2Request represents any request sent to a C2 server by an Agent.
+// Valid reasons: ["register", "task"]
+// Valid agent statuses: ["uninitialized", "inactive", "active"]
+type C2Request struct {
+	AgentID     string `json:"agent_id"`
+	Reason      string `json:"reason"`
+	AgentStatus string `json:"agent_status"`
+	Message     string `json:"message"`
+	Hmac        string `json:"hmac"`
+}
+
+// Initialize a new C2Request with status uninitialized, for use when creating a new agent
+func NewC2Request() C2Request {
+	return C2Request{AgentStatus: "uninitialized"}
+}
+
+// Validates if the C2Request contains valid data
+func (r C2Request) Valid() bool {
+
+	// Validate AgentID is uuid
+	if _, err := uuid.Parse(r.AgentID); err != nil {
+		return false
+	}
+
+	// Validate other fields are one of the valid values
+	return (r.Reason == "register" || r.Reason == "task") &&
+		(r.AgentStatus == "uninitialized" || r.AgentStatus == "inactive" || r.AgentStatus == "active")
+}
+
+// Converts a C2Request into a new Agent for registration
+func (r C2Request) IntoNewAgent() Agent {
+	return Agent{
+		ID:            r.AgentID, // uuid generated at agent computer, sent with initial checkin request
+		Status:        r.AgentStatus,
+		FirstCallback: time.Now(),
+		ModifiedAt:    time.Now(),
+	}
+}
