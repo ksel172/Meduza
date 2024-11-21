d0e9e94f7c1677de43ebe920aa1ffd1bca53d45a
MobasirSarkar <mobasirgithubonly@gmail.com>
2024-11-21 14:47:39 +0530

    updated the routes

--- teamserver/internal/server/routes.go
+++ teamserver/internal/server/routes.go
@@ -10,18 +10,12 @@ func (s *Server) RegisterRoutes() http.Handler {
 
 	router := gin.Default()
 
-	pubRoutes := router.Group("/api/v1/teamserver/public") // public routes
-
-	{
-		pubRoutes.Use(s.HandleCors())
-		pubRoutes.POST("/register", s.dependencies.UserController.AddUsersController)
-		pubRoutes.POST("/add-admin", s.dependencies.AdminController.CreateAdmin)
-	}
-
 	authroutes := router.Group("/api/v1/auth") // auth routes
 
 	{
-		pubRoutes.Use(s.HandleCors())
+		authroutes.Use(s.HandleCors())
+		authroutes.POST("/register", s.dependencies.UserController.AddUsersController)
+		authroutes.POST("/add-admin", s.dependencies.AdminController.CreateAdmin)
 		authroutes.POST("/sign-in", s.dependencies.AuthController.LoginController)
 		authroutes.GET("/refresh-token", s.dependencies.AuthController.RefreshTokenController)
 	}
