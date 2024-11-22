cd4173cb95a86438ad8958dc3bdab4e6c6e8a819
Kobbi Gal <kobbi.gal@gmail.com>
2024-11-22 23:44:11 +0200

    used env util for conf values

--- teamserver/utils/env.go
+++ teamserver/utils/env.go
@@ -1,13 +1,15 @@
 package utils
 
 import (
+	"log"
 	"os"
 	"strconv"
 )
 
-func GetEnvString(key, fallback string) string {
+func GetEnvString(key string, fallback string) string {
 	val, ok := os.LookupEnv(key)
 	if !ok {
+		log.Printf("Environment variable '%s' not set, defaulting to '%s'...\n", key, fallback)
 		return fallback
 	}
 
@@ -17,11 +19,13 @@ func GetEnvString(key, fallback string) string {
 func GetEnvInt(key string, fallback int) int {
 	val, ok := os.LookupEnv(key)
 	if !ok {
+		log.Printf("Environment variable '%s' not set, defaulting to '%d'...\n", key, fallback)
 		return fallback
 	}
 
 	valAsInt, err := strconv.Atoi(val)
 	if err != nil {
+		log.Printf("Environment variable '%s' not an integer, defaulting to '%d'...\n", key, fallback)
 		return fallback
 	}
 
@@ -31,11 +35,13 @@ func GetEnvInt(key string, fallback int) int {
 func GetEnvBool(key string, fallback bool) bool {
 	val, ok := os.LookupEnv(key)
 	if !ok {
+		log.Printf("Environment variable '%s' not set, defaulting to '%t'...\n", key, fallback)
 		return fallback
 	}
 
 	boolVal, err := strconv.ParseBool(val)
 	if err != nil {
+		log.Printf("Environment variable '%s' not a bool, defaulting to '%t'...\n", key, fallback)
 		return fallback
 	}
 
