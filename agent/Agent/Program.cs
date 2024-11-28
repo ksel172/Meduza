f0485250c456ed970995f35cb4964bfab67c770b
Konstantin Seliadtsou <130931089+ksel172@users.noreply.github.com>
2024-11-28 12:12:05 +0200

    Implemented working embedded configuration

--- agent/Agent/Program.cs
+++ agent/Agent/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using Agent.Models;
+using Agent.Services;
+
+AgentInformationService agentInformationService = new AgentInformationService();
+AgentInfo agentInformation = await agentInformationService.GetAgentInfoAsync();
+try
+{
+    var baseConfig = ConfigLoader.LoadEmbeddedConfig();
+    
+    if (agentInformation is not null)
+    {
+        baseConfig.AgentId = agentInformation.AgentId;
+    }
+
+    while (true)
+    {
+
+        await Task.Delay(baseConfig.Sleep * 1000);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Agent initialization failed: {ex.Message}");
+}
