c5fc4fbc6641bbab22a65b352ea152c4d64f9cef
Konstantin Seliadtsou <130931089+ksel172@users.noreply.github.com>
2024-12-11 11:21:11 +0200

    Returned agent folder

---
+++ agent/Agent/AgentTaskExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Agent.Models;
+using System.Collections.Concurrent;
+
+namespace Agent
+{
+    internal static class AgentTaskExtensions
+    {
+        internal static void QueueRunningStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
+        {
+            agentTask.Status = AgentTaskStatus.Running;
+            agentTask.TaskStarted = DateTime.UtcNow;
+            lock (messageQueueLock)
+            {
+                messageQueue.Enqueue(agentTask);
+            }
+        }
+
+        internal static void QueueQueuedStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
+        {
+            agentTask.Status = AgentTaskStatus.Queued;
+            lock (messageQueueLock)
+            {
+                messageQueue.Enqueue(agentTask);
+            }
+        }
+
+        internal static void QueueCompletedStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
+        {
+            agentTask.Status = AgentTaskStatus.Complete;
+            agentTask.TaskCompleted = DateTime.UtcNow;
+            lock (messageQueueLock)
+            {
+                messageQueue.Enqueue(agentTask);
+            }
+        }
+    }
+}
---
+++ agent/Agent/Models/AgentStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace Agent.Models
+{
+    public enum AgentStatus
+    {
+        Uninitialized,
+        Stage0,
+        Stage1,
+        Stage2,
+        Active,
+        Lost,
+        Exited,
+        Disconnected,
+        Hidden
+    }
+}
