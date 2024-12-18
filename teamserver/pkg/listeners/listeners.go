33050e394773812852343ae187d43067b7510368
Konstantin Seliadtsou <130931089+ksel172@users.noreply.github.com>
2024-12-18 13:00:02 +0200

    Fix ListenerType text

--- teamserver/pkg/listeners/listeners.go
+++ teamserver/pkg/listeners/listeners.go
@@ -5,13 +5,13 @@ type ListenerType = string
 type unknown = map[string]any
 
 const (
-	HTTP    ListenerType = "http" // HTTP is a constant for all HTTP listener types (e.g. HTTP/1 and etc.)
-	TCP     ListenerType = "tcp"  // TCP is a constant for TCP bind & reverse listeners.
+	HTTP    ListenerType = "http"
+	TCP     ListenerType = "tcp"
 	FOREIGN ListenerType = "foreign"
-	SMB     ListenerType = "smb" // SMB is a constant for SMB named pipe bind  & reverse listeners.
+	SMB     ListenerType = "smb"
 )
 
-func AllListenerType() []ListenerType {
+func AllListenerTypes() []ListenerType {
 	return []ListenerType{
 		HTTP, TCP, FOREIGN, SMB,
 	}
