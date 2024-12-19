d8180bc86f86297ddf898d655d9c9c6d343506e1
MobasirSarkar <mobasirgithubonly@gmail.com>
2024-12-19 20:47:18 +0530

    test

--- teamserver/internal/storage/dal/agent_dal.go
+++ teamserver/internal/storage/dal/agent_dal.go
@@ -8,7 +8,6 @@ import (
 	"github.com/ksel172/Meduza/teamserver/models"
 )
 
-<<<<<<< HEAD
 type IAgentDAL interface {
 	GetAgent(agentID string) (models.Agent, error)
 	UpdateAgent(ctx context.Context, agent models.UpdateAgentRequest) (models.Agent, error)
@@ -19,8 +18,6 @@ type IAgentDAL interface {
 	DeleteAgentTasks(ctx context.Context, agentID string) error
 }
 
-=======
->>>>>>> b2ff608 (Migrate agents from redis to postgres)
 type AgentDAL struct {
 	db     *sql.DB
 	schema string
