62063f19c0c5a407861ef84ce715603d2a044ca1
mochivi <victormochi1@gmail.com>
2024-12-21 13:45:47 -0300

    add user dal mock

--- teamserver/tests/mocks/dal_mocks.go
+++ teamserver/tests/mocks/dal_mocks.go
@@ -54,3 +54,27 @@ func (m *MockCheckInDal) CreateAgent(ctx context.Context, agent models.Agent) er
 	args := m.Called(agent)
 	return args.Error(0)
 }
+
+type MockUserDAL struct {
+	mock.Mock
+}
+
+func (m *MockUserDAL) AddUsers(ctx context.Context, user *models.ResUser) error {
+	args := m.Called(user)
+	return args.Error(0)
+}
+
+func (m *MockUserDAL) GetUsers(ctx context.Context) ([]models.User, error) {
+	args := m.Called()
+	return args.Get(0).([]models.User), args.Error(1)
+}
+
+func (m *MockUserDAL) GetUserByUsername(ctx context.Context, username string) (*models.ResUser, error) {
+	args := m.Called(username)
+	return args.Get(0).(*models.ResUser), args.Error(1)
+}
+
+func (m *MockUserDAL) GetUserById(ctx context.Context, id string) (*models.ResUser, error) {
+	args := m.Called(id)
+	return args.Get(0).(*models.ResUser), args.Error(1)
+}
