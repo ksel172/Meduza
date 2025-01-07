d6838a06c0ba600b80c7ef3480309f8cf17647ab
Konstantin Seliadtsou <130931089+ksel172@users.noreply.github.com>
2025-01-07 18:18:27 +0200

    Fix http listner config in /models

--- teamserver/models/listener.go
+++ teamserver/models/listener.go
@@ -91,24 +91,21 @@ type ListenerRequest struct {
 }
 
 type HTTPListenerConfig struct {
-	KillDate         int64            `json:"kill_date"`
-	WorkingHours     string           `json:"working_hours"`
-	Hosts            []string         `json:"hosts"`
-	HostBind         string           `json:"host_bind"`
-	HostRotation     string           `json:"host_rotation"`
-	PortBind         string           `json:"port_bind"`
-	PortConn         string           `json:"port_conn"`
-	Secure           bool             `json:"secure"`
-	HostHeader       string           `json:"host_header"`
-	Headers          []Header         `json:"headers"`
-	Uris             []string         `json:"uris"`
-	Certificate      Certificate      `json:"certificate"`
-	WhitelistEnabled bool             `json:"whitelist_enabled"`
-	Whitelist        []string         `json:"whitelist,omitempty"`
-	BlacklistEnabled bool             `json:"blacklist_enabled"`
-	Blacklist        []string         `json:"blacklist,omitempty"`
-	ProxySettings    ProxyConfig      `json:"proxy_settings"`
-	ResponseRules    ResponseSettings `json:"response_rules"`
+	WorkingHours     string        `json:"working_hours"`
+	Hosts            []string      `json:"hosts"`
+	HostBind         string        `json:"host_bind"`
+	HostRotation     string        `json:"host_rotation"`
+	PortBind         string        `json:"port_bind"`
+	PortConn         string        `json:"port_conn"`
+	Secure           bool          `json:"secure"`
+	HostHeader       string        `json:"host_header"`
+	Headers          []Header      `json:"headers"`
+	Uris             []string      `json:"uris"`
+	Certificate      Certificate   `json:"certificate"`
+	WhitelistEnabled bool          `json:"whitelist_enabled"`
+	Whitelist        []string      `json:"whitelist"`
+	BlacklistEnabled bool          `json:"blacklist_enabled"`
+	ProxySettings    ProxySettings `json:"proxy_settings"`
 }
 
 // Validate ensures the configuration is valid before use.
@@ -157,25 +154,22 @@ type Authentication struct {
 	Password string `json:"password,omitempty"`
 }
 
-type Header struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
 type Certificate struct {
 	CertPath string `json:"cert_path"`
 	KeyPath  string `json:"key_path"`
 }
 
-type ProxyConfig struct {
+// ProxySettings represents the configuration for a proxy server.
+type ProxySettings struct {
 	Enabled  bool   `json:"enabled"`
 	Type     string `json:"type"`
-	Host     string `json:"host"`
 	Port     string `json:"port"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-type ResponseSettings struct {
-	Headers []Header `json:"headers"`
+// Header represents a key-value pair used in HTTP headers.
+type Header struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
 }
--- teamserver/models/payload.go
+++ teamserver/models/payload.go
@@ -5,12 +5,11 @@ import "time"
 type PayloadRequest struct {
 	ListenerID string `json:"listenerID"`
 	//ListenerType   string        `json:"listenerType"`
-	ListenerConfig any       `json:"config"`
-	Sleep          int       `json:"sleep"`
-	Jitter         int       `json:"jitter"` // Jitter as a percentage
-	StartDate      time.Time `json:"start_date"`
-	KillDate       time.Time `json:"kill_date"`
-	WorkingHours   [2]int    `json:"working_hours"`
+	Sleep        int       `json:"sleep"`
+	Jitter       int       `json:"jitter"` // Jitter as a percentage
+	StartDate    time.Time `json:"start_date"`
+	KillDate     time.Time `json:"kill_date"`
+	WorkingHours [2]int    `json:"working_hours"`
 }
 
 type PayloadConfig struct {
