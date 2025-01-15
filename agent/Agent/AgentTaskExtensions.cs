80caaf72af1aa581c381ea2abda3aa725d1d6d6d
Konstantin Seliadtsou <130931089+ksel172@users.noreply.github.com>
2025-01-15 12:14:25 +0200

    Remove semaphore for now

--- agent/Agent/AgentTaskExtensions.cs
+++ agent/Agent/AgentTaskExtensions.cs
@@ -1,48 +1,37 @@
 ﻿using Agent.Models;
 using System.Collections.Concurrent;
 
-internal static class AgentTaskExtensions
+namespace Meduza.Agent
 {
-    private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
-
-    internal static void QueueRunningStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
+    internal static class AgentTaskExtensions
     {
-        Console.WriteLine($"Attempting to lock for task {agentTask.Id}");
-        semaphore.Wait();
-        try
+        internal static void QueueRunningStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
         {
             agentTask.Status = AgentTaskStatus.Running;
             agentTask.TaskStarted = DateTime.UtcNow;
-            Console.WriteLine($"Task {agentTask.Id} status set to running");
             lock (messageQueueLock)
             {
                 messageQueue.Enqueue(agentTask);
-                Console.WriteLine($"Task {agentTask.Id} enqueued");
             }
         }
-        finally
-        {
-            semaphore.Release();
-            Console.WriteLine($"Lock released for task {agentTask.Id}");
-        }
-    }
 
-    internal static void QueueQueuedStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
-    {
-        agentTask.Status = AgentTaskStatus.Queued;
-        lock (messageQueueLock)
+        internal static void QueueQueuedStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
         {
-            messageQueue.Enqueue(agentTask);
+            agentTask.Status = AgentTaskStatus.Queued;
+            lock (messageQueueLock)
+            {
+                messageQueue.Enqueue(agentTask);
+            }
         }
-    }
 
-    internal static void QueueCompletedStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
-    {
-        agentTask.Status = AgentTaskStatus.Complete;
-        agentTask.TaskCompleted = DateTime.UtcNow;
-        lock (messageQueueLock)
+        internal static void QueueCompletedStatus(this AgentTask agentTask, ConcurrentQueue<AgentTask> messageQueue, object messageQueueLock)
         {
-            messageQueue.Enqueue(agentTask);
+            agentTask.Status = AgentTaskStatus.Complete;
+            agentTask.TaskCompleted = DateTime.UtcNow;
+            lock (messageQueueLock)
+            {
+                messageQueue.Enqueue(agentTask);
+            }
         }
     }
 }
