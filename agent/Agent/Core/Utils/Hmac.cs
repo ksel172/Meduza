73f71b1bb6acd2102396b1a407a69390bf8b3025
Konstantin Seliadtsou <130931089+ksel172@users.noreply.github.com>
2025-02-02 00:15:22 +0200

    Added hmac utilities in C# and Go

---
+++ agent/Agent/Core/Utils/Hmac.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent.Core.Utils
+{
+    internal class Hmac
+    {
+        private static string GenerateHMAC(byte[] message, byte[] key)
+        {
+            using (HMACSHA256 hmac = new HMACSHA256(key))
+            {
+                byte[] hash = hmac.ComputeHash(message);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        private static bool VerifyHMAC(byte[] message, string receivedHMAC, byte[] key)
+        {
+            string computedHMAC = GenerateHMAC(message, key);
+            return computedHMAC == receivedHMAC;
+        }
+    }
+}
