1882b04108a30395075eceee4e0904105f2aba35
Konstantin Seliadtsou <130931089+ksel172@users.noreply.github.com>
2025-03-06 10:04:02 +0200

    Added controller.sql

--- teamserver/docker/pgsql/queries/controller.sql
+++ teamserver/docker/pgsql/queries/controller.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS {POSTGRES_SCHEMA}.{TABLE_NAME} (
     id UUID PRIMARY KEY,
-    type VARCHAR(10) NOT NULL CHECK (type IN ('cert', 'key')),
-    path VARCHAR(255) NOT NULL,
-    filename VARCHAR(255) NOT NULL,
+    endpoint VARCHAR(255) NOT NULL,
+    public_key BYTEA NOT NULL,
+    private_key BYTEA NOT NULL,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
-    UNIQUE (type)
 );
